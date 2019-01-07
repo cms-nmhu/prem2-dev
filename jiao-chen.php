ef8998f81a5672e127e1ceb819e468d2fdf569fd
ejucov <ejucov@live.nmhu.edu>
2019-01-07 15:55:52 -0700

    upd2019-2

--- jiao-chen.php
+++ jiao-chen.php
@@ -186,7 +186,7 @@
 		<br>
 	  	<div class="row">			
 		  <div class="col-md-3">
-			<img src="images/yuliya-ti.png" width="120px" class="pull-left" style="margin-right:15px">
+			<img src="images/yuliya-ti.jpg" width="120px" class="pull-left" style="margin-right:15px">
 		  </div>
           <div class="col-md-9 content">			
 			<h4 style="display:inline; vertical-align:top; font-weight:bold">Yuliya Ti</h4>
